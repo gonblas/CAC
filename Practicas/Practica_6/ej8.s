124f3872199df79419dc27194ba00a52ccb4a457
gonblas <gonzagabino@gmail.com>
2023-06-06 13:14:47 -0300

    Practica 6 - Terminada

--- Practicas/Practica_6/ej8.s
+++ Practicas/Practica_6/ej8.s
@@ -84,7 +84,7 @@ color_fondo:    .word32 0x00FFFFFF  ; Blanco
                 sd $t0, 32($sp)
 
 
-loop:           daddi $a2, $0, 0    ;Pelota 1 (Pelota 1 tiene numero 0)
+loop:           daddi $a2, $0, 1    ;Pelota 1 (Pelota 1 tiene numero 1)
                 jal pelota
 
                 daddi $a2, $a2, 1   ;Pelota 2
@@ -99,7 +99,7 @@ loop:           daddi $a2, $0, 0    ;Pelota 1 (Pelota 1 tiene numero 0)
                 daddi $a2, $a2, 1   ;Pelota 5
                 jal pelota
 
-                daddi $t0, $0, 500  ; Hace una demora para que el rebote no sea tan rápido.
+                daddi $t0, $0, 50   ; Hace una demora para que el rebote no sea tan rápido.
 demora:         daddi $t0, $t0, -1  ; Esto genera una infinidad de RAW y BTS pero...
                 bnez $t0, demora    ; ¡hay que hacer tiempo igualmente!
                 j loop
@@ -112,65 +112,68 @@ demora:         daddi $t0, $t0, -1  ; Esto genera una infinidad de RAW y BTS per
 pelota:         daddi $sp, $sp, -40
                 sd $s0, 0($sp)
                 sd $s0, 8($sp)
-                sd $s2, 0($sp)
-                sd $s3, 8($sp)
-                sd $s4, 0($sp)
+                sd $s2, 16($sp)
+                sd $s3, 24($sp)
+                sd $s4, 32($sp)
 
                 daddi $t0, $0, 0x400         ;Calculo la direccion donde estan los datos
-                daddi $t1, $0, 40
-                dmul $t1, $a2, $t1   
-                dadd $t0, $t0, $t1
+                daddi $t3, $a2, 0            ;Contador para la resta
+
+resta:          daddi $t0, $t0, -40
+                daddi $t3, $t3, -1
+                bnez $t3, resta
+
 
                 ;color -> posX -> posY -> dirX -> dirY
-                lwu $v0, 0($t0)              ; Color
-                lb $s0, 8($t0)               ; Coordenada X de la pelota
-                lb $s1, 16($t0)              ; Coordenada Y de la pelota
-                lb $s2, 24($t0)              ; Dirección X de la pelota
-                lb $s3, 32($t0)              ; Dirección X de la pelota
-                daddi $s4, $0, 5             ; Comando para dibujar un punto
-
-
-                sw $a3, 0($s7)               ; Borra la pelota
-                sb $s0, 4($a1)
-                sb $s1, 5($a1)
+                lb $s3, 0($t0)              ; Dirección Y de la pelota
+                lb $s2, 8($t0)              ; Dirección X de la pelota
+                lb $s1, 16($t0)             ; Coordenada Y de la pelota
+                lb $s0, 24($t0)             ; Coordenada X de la pelota
+                lwu $v0, 32($t0)            ; Color
+                daddi $s4, $0, 5            ; Comando para dibujar un punto
+                ;Hasta aca anda bien
+                sw $a3, 0($a1)              ; Borra la pelota (pos ant del color de fondo)
+                sb $s0, 5($a1)
+                sb $s1, 4($a1)
                 sd $s4, 0($a0)
 
                 dadd $s0, $s0, $s2           ; Mueve la pelota en la dirección actual
                 dadd $s1, $s1, $s3
 
                 daddi $t1, $0, 48            ; Comprueba que la pelota no esté en la columna de más
-                slt $t0, $t1, $s0            ; a la derecha. Si es así, cambia la dirección en X.
-                dsll $t0, $t0, 1
-                dsub $s2, $s2, $t0
+                slt $t4, $t1, $s0            ; a la derecha. Si es así, cambia la dirección en X.
+                dsll $t4, $t4, 1
+                dsub $s2, $s2, $t4
 
-                slt $t0, $t1, $s1            ; Comprueba que la pelota no esté en la fila de más arriba.
-                dsll $t0, $t0, 1             ; Si es así, cambia la dirección en Y.
-                dsub $s3, $s3, $t0
+                slt $t4, $t1, $s1            ; Comprueba que la pelota no esté en la fila de más arriba.
+                dsll $t4, $t4, 1             ; Si es así, cambia la dirección en Y.
+                dsub $s3, $s3, $t4
 
-                slti $t0, $s0, 1             ; Comprueba que la pelota no esté en la columna de más
-                dsll $t0, $t0, 1             ; a la izquierda. Si es así, cambia la dirección en X.
-                dadd $s2, $s2, $t0
+                slti $t4, $s0, 1             ; Comprueba que la pelota no esté en la columna de más
+                dsll $t4, $t4, 1             ; a la izquierda. Si es así, cambia la dirección en X.
+                dadd $s2, $s2, $t4
 
-                slti $t0, $s1, 1             ; Comprueba que la pelota no esté en la fila de más abajo.
-                dsll $t0, $t0, 1             ; Si es así, cambia la dirección en Y.
-                dadd $s3, $s3, $t0
+                slti $t4, $s1, 1             ; Comprueba que la pelota no esté en la fila de más abajo.
+                dsll $t4, $t4, 1             ; Si es así, cambia la dirección en Y.
+                dadd $s3, $s3, $t4
 
 
-                sw $v0, 0($s1)               ; Dibuja la pelota.
-                sb $s0, 4($s1)
-                sb $s1, 5($s1)
-                sd $s4, 0($s0)
+                sw $v0, 0($a1)               ; Dibuja la pelota.
+                sb $s0, 5($a1)
+                sb $s1, 4($a1)
+                sd $s4, 0($a0)
 
                 ;Resguardo valores de la pelota que pueden ser modificados
-                sd  $s0, 8($t0)              ; Guardo coordenada X de la pelota
-                sd  $s1, 16($t0)             ; Guardo coordenada Y de la pelota
-                sd  $s2, 24($t0)             ; Guardo dirección X de la pelota
-                sd  $s3, 32($t0)             ; Guardo dirección X de la pelota
-
+                sd  $s3, 0($t0)              ; Guardo coordenada X de la pelota
+                sd  $s2, 8($t0)              ; Guardo coordenada Y de la pelota
+                sd  $s1, 16($t0)             ; Guardo dirección X de la pelota
+                sd  $s0, 24($t0)             ; Guardo dirección X de la pelota
+                sd  $v0, 32($t0)             ; Guardo dirección X de la pelota
+                
                 ld $s0, 0($sp)
                 ld $s0, 8($sp)
-                ld $s2, 0($sp)
-                ld $s3, 8($sp)
-                ld $s4, 0($sp)
+                ld $s2, 16($sp)
+                ld $s3, 24($sp)
+                ld $s4, 32($sp)
                 daddi $sp, $sp, 40
                 jr $ra
